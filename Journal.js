19741eff770d883a34df121fcb23a52beb73c406
raut amol <84304332+amolraut474@users.noreply.github.com>
2024-04-01 19:01:33 +0530

    Add files via upload

---
+++ Journal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import BlogList from '../Compontas/Journal_Com/Blog_list';
+import Getintouch from '../Compontas/About_Com/Getintouch';
+export default function Journal() {
+  return (
+    <div>
+    <div> <BlogList/></div> 
+    <div><Getintouch /></div>
+    </div>
+  )
+}
